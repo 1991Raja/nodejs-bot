433df1063baf98e3acc608f6611f6e2e637eb7c7
1991Raja <124337842+1991Raja@users.noreply.github.com>
2023-02-03 16:51:42 +0530

    Add files via upload

---
+++ [4]data.js
@@ -0,0 +1,28 @@
+module.exports = {
+  token:'6021864764:AAEaOD0zC62aFjA1PW4A2wPviVRn9nDAusM', // token from @BotFather
+  mongoLink: 'mongodb+srv://9780Raja:9780471991@cluster0.w6o2zup.mongodb.net/?retryWrites=true&w=majority', // mongo-link from cloud.mongodb.com
+  admin: '5242589887', // admin`s id for sending him/her errors,
+  currency:'Matic',
+  min_wd:0.01,// minimum withdrawal amount
+  refer_bonus:0.003,
+  joinbal: 0.001,
+  max_wd:0.02,
+  payment_channel:'@sachin_richu_bodejs',
+  channel: '*👋 Hello ~ Complete Telegram Task:*\n\n🔹 Join *Unicorn Drops* [Channel](https://t.me/AirdropSearchTeam) *And* [Group](https://t.me/AirdropSearchTeam)\n🔹 Join *Airdrops Hint* [Channel](https://t.me/Airdrop_Fam) *and* [Group](https://t.me/Airdrop_Fam)\n*🚊 Click "Check" button if done...*',
+  welcome:'*🔰 PerfectWorld Giveaway *\n*🎁 Reward Pool: 5,000,000,000 $PW*\n*👥 Referral: 500000 $PW*\n*🔥 Market: MEXC , QuickSwap*\n*📛 Winners:* For all valid users \n*🧶 Rate: ⭐️⭐️⭐️⭐️⭐️*\n*🚀 Distribution Date:* Instant to Wallet \n\n*📚 All the Tasks are Mandatory* \n     *🔹 @AirdropSearchTeam*\n     *🔹 @AirdropSearchTeam*\n     *🔹 @Airdrop_Fam*\n     *🔹 @Airdrop_Fam\n*♦️ @sachin_richu_bodejs (—payout—)*\n\n*💧 Contract:* `0xb4228798fF437ecD8fa43429664e9992256fe6Ac`\n\n*📡 Website: soon...*',
+  details:'*▪️ Network:* Polygon\n*▪️ Contract Address:* `0xaaa5b9e6c589642f98a1cda99b9d024b8407285a`\n*▪️ Name:* IRON Titanium\n*▪️ Symbol:* TITAN\n*▪️ Decimals:* 18\n\n*📊 Market:* QuickSwap, MEXC',
+  welcomepic: 'https://te.legra.ph/file/a8bd14b51495f5ca2102e.jpg',
+  media: '*👋 Hello ~ Complete YouTube Task:*\n\n🔹 Follow *Unicorn Drops™️* on [YouTube Channel](https://youtube.com/channel/UCdYMvnmu0YnNFl_V6Db1L_w) & Like and Comment `Good` on All Videos \n\n *📝 Submit your YouTube Link..... *',
+  addtext: '*💧 Submit Your Polygon (MATIC) address*\n\n‼️ *You can find it on Trust Wallet *(Do not use any exchange Address)',
+  addimage: 'https://te.legra.ph/file/edfba3a641ccab7d72e70.jpg',
+  symbol:'PW',
+  comment: '*👋Hello ~ Complete Optional Task:*\n\n🔹 Join Our *Promoters Telegram* [Channel1](https://t.me/AirdropRampage) , [Channel2](t.me/channel) , [Channel3](t.me/channel)\n\n✅ And Click *Done☑️* Or You Can *Skip ⏭️* Also',
+  bot_username:'@PerfectWorldAirdropBot',
+  channels: ['@AirdropSearchTeam'],
+  channel1: '@AirdropSearchTeam',
+  channel2: '@Airdrop_Fam',
+  channel3: '@sachin_richu_bodejs',
+  channel4: '@Airdrop_Fam',
+  channel5: '@Airdrop_Fam',
+}
+
